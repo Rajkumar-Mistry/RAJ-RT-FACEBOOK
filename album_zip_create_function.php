0d4928c21cb6915b2c2a30373ad2833ab95e3f94
RAJ MISTRY <42669060+Rajkumar-Mistry@users.noreply.github.com>
2018-09-13 00:54:33 +0530

    Update album_zip_create_function.php

--- album_zip_create_function.php
+++ album_zip_create_function.php
@@ -86,6 +86,20 @@ foreach ($files as $name => $file)
     }
 }
 $zip->close();
+	
+$zip_path = $main_folder;
+$zip_name = "raj";
+header( "Pragma: public" );
+header( "Expires: 0" );
+header( "Cache-Control: must-revalidate, post-check=0, pre-check=0" );
+header( "Cache-Control: public" );
+header( "Content-Description: File Transfer" );
+header( "Content-type: application/zip" );
+header( "Content-Disposition: attachment; filename=\"" . $zip_name . "\"" );
+header( "Content-Transfer-Encoding: binary" );
+header( "Content-Length: " . filesize( $zip_path ) );
+
+readfile( $zip_path );
 }
 function zip_download_pc($main_folder,$zipname)
 {
