7cdbcff6ad506a137869e69decf95d34bd0bb945
RAJ MISTRY <42669060+Rajkumar-Mistry@users.noreply.github.com>
2018-09-13 18:18:53 +0530

    Update album.php

--- album.php
+++ album.php
@@ -208,7 +208,7 @@ include 'album_zip_create_function.php';
 
 				 echo "<div class=\"col-md-6 col-sm-6 col-xs-12\">";
                          //echo "<div class=\"panel-body\"> <a href='slide_show.php?album_name=$key'>SLIDE SHOW</a></div>";
-			    echo "<div class=\"panel-body\"><a onclick='<script type='text/javascript'>start();</script>'>Open Modal</a></div>";
+			    echo "<div class=\"panel-body\"><a onclick=start();>Open Modal</a></div>";
 			    
 			   
 			 
