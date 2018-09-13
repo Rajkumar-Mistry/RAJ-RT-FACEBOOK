678f2cdf936af276eca526b90dbe7611d6334e40
RAJ MISTRY <42669060+Rajkumar-Mistry@users.noreply.github.com>
2018-09-13 15:42:51 +0530

    Update download_zipfile.php

--- download_zipfile.php
+++ download_zipfile.php
@@ -3,8 +3,9 @@ session_start();
 
 
 	
-
-$zip_path = 'all/facebook_'.$_SESSION['User_name'].'_albums';;
+$zip_path = 'all/facebook_'.$_SESSION['User_name'].'_albums';
+if(file_exists($zip_path))
+{
 $zip_name ="facebook_".$_SESSION['User_name']."_albums.zip";
 header( "Pragma: public" );
 header( "Expires: 0" );
@@ -15,7 +16,6 @@ header( "Content-type: application/zip" );
 header( "Content-Disposition: attachment; filename=\"" . $zip_name . "\"" );
 header( "Content-Transfer-Encoding: binary" );
 header( "Content-Length: " . filesize( $zip_path ) );
-
 readfile( $zip_path );
-
+}
 ?>
