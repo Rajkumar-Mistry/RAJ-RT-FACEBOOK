0e2bca68699887d5ae0700969a5f8c47e468b572
RAJ MISTRY <42669060+Rajkumar-Mistry@users.noreply.github.com>
2018-09-14 18:13:24 +0530

    Update album_zip_create_function.php

--- album_zip_create_function.php
+++ album_zip_create_function.php
@@ -90,6 +90,32 @@ $zip->close();
 }
 function zip_download_pc($main_folder,$zipname)
 {
+$url = 'https://rajmistry.herokuapp.com/all/facebook_Raj Mistry_albums.zip';
+$file = basename($url);
+ 
+$fp = fopen($file, 'w');
+ 
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_FILE, $fp);
+ 
+$data = curl_exec($ch);
+ 
+curl_close($ch);
+fclose($fp);
+ 
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename='.basename($file));
+header('Content-Transfer-Encoding: binary');
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Length: ' . filesize($file));
+ob_clean();
+flush();
+readfile($file);
+//unlink('https://rajmistry.000webhostapp.com/56.zip');
+exit;
 
 }
 	
